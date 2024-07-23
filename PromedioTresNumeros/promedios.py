3445c384c8eb7b143aa5aa2a54cceda752f06bea
Lenna <danna.coello@tecnologicoliceocristiano.edu.ec>
2024-07-22 21:34:43 -0500

    Promedio/3

---
+++ PromedioTresNumeros/promedios.py
@@ -0,0 +1,8 @@
+N1 = int(input("Ingresar nota1: , "))
+N2 = int(input("Ingresar nota2: , "))
+N3 = int(input("Ingresar nota3: , "))
+Suma = N1 + N2 + N3
+Promedio = Suma/3 
+print(Suma) 
+print(Promedio)
+
